3fa5933ee078869fbf394c1e291c96f045728092
Rakesh Goswami <theunmarwadi@gmail.com>
2020-05-26 17:05:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+//variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+// cart
+let cart = [];
+
+// getting the product
+class Products{
+    async getProducts(){
+        try {
+         let result = await fetch('products.json');
+         return data;
+        } catch (error) {
+            console.log(error);
+        }
+    
+    }
+}
+// display products
+class UI {
+
+}
+
+// local storage
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
+
+    // get all products
+    products.getProducts().then(doat => console.log(data));
+});
